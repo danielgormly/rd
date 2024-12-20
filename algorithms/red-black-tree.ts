d29994b1ce7747e6b60d7d3f76f1b9e0d4e01507
Daniel Gormly <danielgormly@hey.com>
2024-12-20 11:30:07 +1100

    Stripped rb tree

--- algorithms/red-black-tree.ts
+++ algorithms/red-black-tree.ts
@@ -24,59 +24,9 @@ class RedBlackNode {
 class RedBlackTree {
   root: RedBlackNode | null = null;
   constructor() {}
-  // Black aunt rotate
-  // Red aunt colourflip
-  rebalance(node: RedBlackNode) {
-    if (node.parent && node.parent.parent) {
-      if (node.parent.parent.right === node) {
-        // left uncle
-      } else {
-        // right uncle
-      }
-    }
-  }
-  insert(val: number) {
-    if (!this.root) {
-      this.root = new RedBlackNode(val, "black", true);
-      return;
-    }
-    const _insert = (curNode: RedBlackNode, node: RedBlackNode) => {
-      if (val === curNode.val) {
-        return console.warn("Discarding duplicate node");
-      }
-      if (val < curNode.val) {
-        if (!curNode.left) {
-          curNode.left = node;
-          node.parent = curNode;
-          this.rebalance(node);
-        }
-      }
-      if (val > curNode.val) {
-        if (!curNode.right) {
-          curNode.right = node;
-          node.parent = curNode;
-          this.rebalance(node);
-        }
-      }
-    };
-    const newNode = new RedBlackNode(val, "red");
-    _insert(this.root, newNode);
-  }
-  search(val: number) {
-    const _search = (node: RedBlackNode | null) => {
-      if (node === null) return undefined;
-      if (val === node.val) {
-        return node;
-      }
-      if (val < node.val) {
-        return _search(node.left);
-      }
-      if (val > node.val) {
-        return _search(node.right);
-      }
-    };
-    _search(this.root);
-  }
+  rebalance(node: RedBlackNode) {}
+  insert(val: number) {}
+  search(val: number) {}
   remove(val: number) {}
 }
 
