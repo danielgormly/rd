f0634d0e1ea4ff597bec80a0d37f74ac2ed76b89
Daniel Gormly <danielgormly@hey.com>
2025-03-06 14:52:19 +1100

    Setup

---
+++ webgpu-fundamentals/src/01-triangle.ts
@@ -0,0 +1,9 @@
+export function triangle(el: HTMLElement) {
+  console.log("triangle", el);
+}
+
+export const Triangle = {
+  title: "Triangle",
+  description: "Rendering a triangle",
+  func: triangle,
+};
--- webgpu-fundamentals/src/main.ts
+++ webgpu-fundamentals/src/main.ts
@@ -1,9 +1,52 @@
+import { triangle } from "./01-triangle";
 import "./style.css";
+
+interface Scene {
+  title: string;
+  description: string;
+  func: (el: HTMLElement) => void;
+}
+
+const scenes = new Map<string, Scene>([
+  [
+    "triangle",
+    {
+      title: "Triangle",
+      description: "Rendering a triangle",
+      func: triangle,
+    },
+  ],
+]);
+
+function route(hash: string) {
+  const board = document.getElementById("board");
+  if (!board) return;
+  if (!hash) {
+    const exp = scenes.get("triangle");
+    if (!exp) return;
+    exp.func(board);
+  }
+}
+
+window.addEventListener("hashchange", (event) => {
+  const { hash } = new URL(event.newURL);
+  route(hash);
+});
+
+const links: string[] = [];
+scenes.forEach((exp, key) => {
+  links.push(`<li><a href="#${key}">${exp.title}</a>: ${exp.description}</li>`);
+});
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
-  <h1>WebGL</h1>
-  <section>
+  <h1>WebGPU Experiments</h1>
+  <section id="board">
   </section>
+  <ul>
+    ${links.join("\n")}
+  </ul>
   </div>
 `;
+
+route(window.location.hash);
