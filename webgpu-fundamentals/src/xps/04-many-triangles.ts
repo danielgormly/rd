997147b8fccb6dbd2998320e8c884dca700b49de
Daniel Gormly <danielgormly@hey.com>
2025-03-12 12:26:53 +1100

    a triangle

--- webgpu-fundamentals/src/xps/04-many-triangles.ts
+++ webgpu-fundamentals/src/xps/04-many-triangles.ts
@@ -32,9 +32,37 @@ struct OurStruct {
 `,
   });
 
+function createUniformBuffer(
+  device: GPUDevice,
+): [
+  GPUBuffer,
+  Float32Array<ArrayBuffer>,
+  { color: number; scale: number; offset: number },
+] {
+  const uniformBufferSize =
+    4 * 4 + // color is 32f (4xrgba)
+    2 * 4 + // scale is 2x32f (x,y)
+    2 * 4; // offset is 2x32f (x,y)
+  const uniformBuffer = device.createBuffer({
+    size: uniformBufferSize,
+    usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+  });
+  const uniformValues = new Float32Array(uniformBufferSize / 4); // byte size to float32 size (4 bytes each)
+  // byte offsets
+  const offsets = {
+    color: 0,
+    scale: 4,
+    offset: 6,
+  };
+  uniformValues.set([0, 1, 0, 1], offsets.color); // set the color
+  uniformValues.set([-0.5, -0.25], offsets.offset); // set the offset
+  return [uniformBuffer, uniformValues, offsets];
+}
+
 export async function manyTriangles(el: HTMLElement) {
   const pane = new Pane();
   const [ctx, device, format] = await initWGPUCanvas(el, true);
+
   const shader = createShader(device);
   const pipeline = device.createRenderPipeline({
     label: "hardcoded checkerboard triangle pipeline",
@@ -46,6 +74,11 @@ export async function manyTriangles(el: HTMLElement) {
       module: shader,
       targets: [{ format }], // pos 0 aligns with fragment shader output (location 0) ?
     },
+  });
+  const [uniformBuffer, uniformValues, offsets] = createUniformBuffer(device);
+  const bindGroup = device.createBindGroup({
+    layout: pipeline.getBindGroupLayout(0),
+    entries: [{ binding: 0, resource: { buffer: uniformBuffer } }],
   });
   // the texture we will render to
   const triangleRenderPassDescriptor = (
@@ -62,12 +95,16 @@ export async function manyTriangles(el: HTMLElement) {
     ],
   });
   function render() {
+    const aspect = ctx.canvas.width / ctx.canvas.height;
+    uniformValues.set([0.5 / aspect, 0.5], offsets.scale);
+    device.queue.writeBuffer(uniformBuffer, 0, uniformValues);
     const renderPassDescriptor = triangleRenderPassDescriptor(
       ctx.getCurrentTexture().createView(),
     );
     const encoder = device.createCommandEncoder({ label: "our encoder " });
     const pass = encoder.beginRenderPass(renderPassDescriptor);
     pass.setPipeline(pipeline);
+    pass.setBindGroup(0, bindGroup);
     pass.draw(3); // call our vertex shader 3 times
     pass.end();
 
