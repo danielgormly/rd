7d343a47e8582f6bb29457564eb36f34ccd76a9f
Daniel Gormly <danielgormly@hey.com>
2025-05-09 14:11:08 +1000

    8.3 rust book

--- rust/collections/src/main.rs
+++ rust/collections/src/main.rs
@@ -66,8 +66,24 @@ fn strings() {
 }
 
 // hash maps (kv)
+use std::collections::HashMap;
+
+fn hash_maps() {
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+    scores.entry(String::from("Red")).or_insert(25);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+    println!("{} score is {} (retrieved individually)", team_name, score);
+    for (key, value) in &scores {
+        println!("{} score is {}", key, value);
+    }
+}
 
 fn main() {
     vectors();
-    strings()
+    strings();
+    hash_maps();
 }
