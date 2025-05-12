a65c8326f25258f6300004648c45c27b23c0d397
Daniel Gormly <danielgormly@hey.com>
2025-05-12 19:56:37 +1000

    11.1 rust book

--- rust/tess/src/lib.rs
+++ rust/tess/src/lib.rs
@@ -2,13 +2,57 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn exploration() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 3,
+            height: 4,
+        };
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_can_not_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 3,
+            height: 4,
+        };
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    #[should_panic(expected = "BRR")]
+    fn greater_than_100() {
+        panic!("BRR")
     }
 }
