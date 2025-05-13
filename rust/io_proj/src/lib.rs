cbf9c63588a8f968ade892f5cb5ab6a940f953f4
Daniel Gormly <danielgormly@hey.com>
2025-05-13 15:01:04 +1000

    Rust book 12.05

--- rust/io_proj/src/lib.rs
+++ rust/io_proj/src/lib.rs
@@ -5,7 +5,9 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     println!("Searching for '{}' in '{}'", config.query, config.file_path);
 
     let contents = fs::read_to_string(config.file_path)?;
-    println!("With text:\n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
     Ok(())
 }
 
@@ -23,5 +25,30 @@ impl Config {
         let file_path = args[2].clone();
 
         Ok(Config { query, file_path })
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "DUCK";
+        let contents = "\
+Rust:
+safe, fast, proDUCKtive.
+Duck you, idiot.";
+        assert_eq!(vec!["safe, fast, proDUCKtive."], search(query, contents));
     }
 }
