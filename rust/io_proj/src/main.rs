73385493b8f0fa54f909d0dd60eeab40557cf9ba
Daniel Gormly <danielgormly@hey.com>
2025-05-13 14:45:25 +1000

    10.3 rust book

--- rust/io_proj/src/main.rs
+++ rust/io_proj/src/main.rs
@@ -1,16 +1,17 @@
 // https://doc.rust-lang.org/stable/book/ch12-00-an-io-project.html
 // building a simplified grep clone
 // Usage: cargo run -- searchstring test/file.txt
-use std::{env, fs};
+use io_proj::{Config, run};
+use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    // args[1] is the binary
-    let query = &args[1];
-    let file_path = &args[2];
-    println!("Searching for '{query}' in '{file_path}'");
-
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1)
+    });
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
