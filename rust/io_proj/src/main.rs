2ab54c5509b52678d7f506d3c5220a5fc9272138
Daniel Gormly <danielgormly@hey.com>
2025-05-13 13:45:32 +1000

    Rust book 12.2

--- rust/io_proj/src/main.rs
+++ rust/io_proj/src/main.rs
@@ -1,7 +1,7 @@
 // https://doc.rust-lang.org/stable/book/ch12-00-an-io-project.html
 // building a simplified grep clone
 // Usage: cargo run -- searchstring test/file.txt
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,4 +9,8 @@ fn main() {
     let query = &args[1];
     let file_path = &args[2];
     println!("Searching for '{query}' in '{file_path}'");
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
