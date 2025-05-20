79edb8119f834960ab113aa67028bd749831f6be
Daniel Gormly <danielgormly@hey.com>
2025-05-20 14:27:09 +1000

    17.03 rust book

--- rust/asynchronous/src/main.rs
+++ rust/asynchronous/src/main.rs
@@ -1,4 +1,7 @@
-use std::time::Duration;
+use std::{
+    pin::{Pin, pin},
+    time::Duration,
+};
 
 use trpl::{Either, Html};
 
@@ -113,6 +116,47 @@ fn concurrency3() {
     });
 }
 
+fn concurrency4() {
+    trpl::run(async {
+        let (tx, mut rx) = trpl::channel();
+        let tx1 = tx.clone();
+        let tx1_fut = pin!(async move {
+            let vals = vec![
+                String::from("hi"),
+                String::from("from"),
+                String::from("the"),
+                String::from("future"),
+            ];
+
+            for val in vals {
+                tx1.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        });
+        let tx_fut = pin!(async move {
+            let vals = vec![
+                String::from("more"),
+                String::from("messages"),
+                String::from("received"),
+                String::from("over"),
+            ];
+
+            for val in vals {
+                tx.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        });
+        let rx_fut = pin!(async {
+            while let Some(value) = rx.recv().await {
+                println!("Got: {value}");
+            }
+        });
+        let futures: Vec<Pin<&mut dyn Future<Output = ()>>> = vec![tx_fut, tx1_fut, rx_fut];
+        trpl::join_all(futures).await;
+        // println!("{a_result}, {b_result}, {c_result}");
+    });
+}
+
 fn main() {
-    concurrency3();
+    concurrency4();
 }
