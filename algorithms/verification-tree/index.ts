97e6d71b9b518a9aaf6592d0186ceb6e73c9d789
Daniel Gormly <danielgormly@hey.com>
2025-08-07 21:59:53 +1000

    Verification tree update

--- algorithms/verification-tree/index.ts
+++ algorithms/verification-tree/index.ts
@@ -24,23 +24,29 @@ const yearLike = 50_000_000_000; // 365 * 24 * 3600 * 1000 (nearest {5,1}x10^x) 
 // Merkle/checksum tree shines in this context, answering the question: what is missing or what is corrupt efficiently
 // in any case - we need to keep track of windows of items, a count, and a check of how timestamps
 
+// TODO: Full rebuilds could be put in a worker
 class ChecksumTree {
-  // TODO: EVERY item with this the finest hash granularity needs to stay here (perhaps an idb/sqlite index is needed - based on server timestamp)
-  // TODO: A rebuild could be put in a worker
-  // TODO: Clearing a day!!!
-  daysTouched = new Set<number>(); // if there's a size it means it's dirty!
+  // Pending changes
+  daysTouched = new Set<number>();
+  // Values <absolute start date of period, checksums>
   years = new Map<number, number>();
   months = new Map<number, number>();
-  days = new Map<number, number>(); // day, checksum of microsecond timestamps
-  // Expects every entry for that day, and validates that they are for that day!
+  days = new Map<number, number>();
+  // Cached relationships
+  monthDays = new Map<number, Set<number>>();
+  yearMonths = new Map<number, Set<number>>();
   clearDay(dayBucket: number) {
     this.insertDay(dayBucket, []);
   }
+  dirty() {
+    return this.daysTouched.size > 0;
+  }
+  // Expects every entry for that day, and validates that they are for that day!
   insertDay(dayBucket: number, usecs: number[]) {
     const daysTouched: number[] = [];
     const checksum = usecs.reduce((xor, usec) => {
       const ms = usec / 1000;
-      const day = Math.floor(ms / dayLike);
+      const day = Math.floor(ms / dayLike) * dayLike;
       if (day !== dayBucket) {
         throw new Error(
           `${usec} does not belong in day bucket in checksum tree`,
@@ -57,8 +63,8 @@ class ChecksumTree {
     let years = new Map<number, number>();
     // Calculate checksum for each month
     this.daysTouched.forEach((dayMs) => {
-      const year = Math.floor(dayMs / yearLike);
-      const month = Math.floor(dayMs / monthLike);
+      const year = Math.floor(dayMs / yearLike) * yearLike;
+      const month = Math.floor(dayMs / monthLike) * monthLike;
       const dayChecksum = this.days.get(dayMs);
       if (typeof dayChecksum !== "number") {
         throw new Error("Checksum tree failed. dayChecksum not found.");
@@ -69,22 +75,34 @@ class ChecksumTree {
       // Update year checksum
       const yearChecksum = years.get(year);
       years.set(year, dayChecksum ^ (yearChecksum || 0));
+      // Update relationships
+      if (!this.monthDays.has(month)) {
+        this.monthDays.set(month, new Set());
+      }
+      this.monthDays.get(month)!.add(dayMs);
+      if (!this.yearMonths.has(year)) {
+        this.yearMonths.set(year, new Set());
+      }
+      this.yearMonths.get(year)!.add(month);
     });
+    // Update months & years
     for (let entry of months.entries()) {
       this.months.set(entry[0], entry[1]);
     }
     for (let entry of years.entries()) {
       this.years.set(entry[0], entry[1]);
     }
+    this.daysTouched.clear();
   }
 }
 
 const tree = new ChecksumTree();
-// const randomDays = Math.random() * 1000 * 1000 * 60 * 60 * 24;
-for (let i = 0; i < 100; i++) {
+
+// Testing a million items
+for (let i = 0; i < 1000; i++) {
   const usecArr: number[] = [];
   const day = Date.now() - i * 1000 * 60 * 60 * 24;
-  const dayBucket = Math.floor(day / dayLike);
+  const dayBucket = Math.floor(day / dayLike) * dayLike;
   for (let i = 0; i < 100; i++) {
     const usecRand = Math.floor(day * 1000 - Math.random() * 1000 * 1000);
     usecArr.push(usecRand);
@@ -92,4 +110,4 @@ for (let i = 0; i < 100; i++) {
   tree.insertDay(dayBucket, usecArr);
 }
 tree.commit();
-console.log(tree);
+console.log(tree.monthDays);
